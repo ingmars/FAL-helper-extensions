f9280cca037a7bcae84e7ae70406153b35a035d0
Ingmar Schlecht <ingmar@typo3.org>
2011-10-19 12:15:09 +0200

    [TASK] Removed the "file" extension from Github as it's now part of the Core as sysext/file/ in the Incubator branch project-fileabstraction, since we need it i.e. for the indexing configuration module.

--- file/ext_tables.sql
+++
@@ -1,7 +0,0 @@
-#
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-	tx_file_images_overlayed int(11) DEFAULT '0' NOT NULL
-	tx_file_images_list int(11) DEFAULT '0' NOT NULL
-);
